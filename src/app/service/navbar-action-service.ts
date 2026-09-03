3e8b70c955e1e70436ad0b5a04cb8c1ecbfa89cc
MrAkashTyagi <34620687+MrAkashTyagi@users.noreply.github.com>
2026-09-04 01:11:42 +0530

    Update navbar-action-service.ts

--- src/app/service/navbar-action-service.ts
+++ src/app/service/navbar-action-service.ts
@@ -1,5 +1,5 @@
-import { Injectable, signal } from '@angular/core';
-import { Subject } from 'rxjs';
+import { Injectable, signal } from "@angular/core";
+import { Subject } from "rxjs";
 
 @Injectable({
   providedIn: 'root',
@@ -8,10 +8,24 @@ export class NavbarActionService {
 
   searchQuery = signal<string>('');
 
+  totalGuestCount = signal<number>(0);
+
+  // totalGuestCount = signal<number>(0);
+
+countLabel = signal<string>('Total Guests');
+
+
   private addClickSubject = new Subject<void>();
   addClick$ = this.addClickSubject.asObservable();
 
-  triggerAddClick(){
+  private exportClickSubject = new Subject<void>();
+  exportClick$ = this.exportClickSubject.asObservable();
+
+  triggerAddClick() {
     this.addClickSubject.next();
+  }
+
+  triggerExportClick() {
+    this.exportClickSubject.next();
   }
 }
