ae5bcbb9b511a368ad2365dc69e97707e350db3d
MrAkashTyagi <34620687+MrAkashTyagi@users.noreply.github.com>
2026-09-04 01:07:10 +0530

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,20 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
 
+import {
+  Chart,
+  PieController,
+  ArcElement,
+  Tooltip,
+  Legend
+} from 'chart.js';
+
+Chart.register(
+  PieController,
+  ArcElement,
+  Tooltip,
+  Legend
+);
+
 bootstrapApplication(App, appConfig)
   .catch((err) => console.error(err));
