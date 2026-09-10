abd1e33d7ea3e6ceaccbc0e7c2308574bebae973
MrAkashTyagi <34620687+MrAkashTyagi@users.noreply.github.com>
2026-09-10 18:05:55 +0530

    Create auth-service.ts

---
+++ src/app/service/auth-service.ts
@@ -0,0 +1,78 @@
+import { HttpClient } from '@angular/common/http';
+import {
+  Injectable,
+  inject,
+  signal
+} from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  private http = inject(HttpClient);
+
+  register(payload: any) {
+    return this.http.post(
+      'http://localhost:8090/auth/register',
+      payload
+    );
+  }
+
+  login(payload: any) {
+    return this.http.post(
+      'http://localhost:8090/auth/login',
+      payload
+    );
+  }
+
+  isLoggedIn(): boolean {
+
+    return !!localStorage.getItem(
+      'user'
+    );
+
+  }
+
+
+  currentUser = signal<any>(null);
+
+  setUser(user: any): void {
+
+    localStorage.setItem(
+      'user',
+      JSON.stringify(user)
+    );
+
+    this.currentUser.set(user);
+
+  }
+
+
+  loadUser(): void {
+
+    if (typeof window === 'undefined') {
+      return;
+    }
+
+    const user =
+      localStorage.getItem('user');
+
+    this.currentUser.set(
+      user ? JSON.parse(user) : null
+    );
+
+  }
+
+  logout(): void {
+
+    if (typeof window !== 'undefined') {
+
+      localStorage.removeItem('user');
+
+    }
+
+    this.currentUser.set(null);
+
+  }
+}
