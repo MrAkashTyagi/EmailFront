dc8b9f02448bb30d997137ea84bc4fd99a930031
MrAkashTyagi <34620687+MrAkashTyagi@users.noreply.github.com>
2026-09-10 18:15:45 +0530

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,46 +6,64 @@ import { AddGuestComponent } from './components/add-guest/add-guest';
 import { Family } from './components/family/family';
 import { Expense } from './expense/expense';
 import { Dashboard } from './components/dashboard/dashboard';
+import { Register } from './components/register/register';
+import { Login } from './components/login/login';
+import { authGuard } from './auth-guard';
 
 
 
 export const routes: Routes = [
 
     {
-        path:"sendemail",
-        component:Email,
-        pathMatch:"full"
+        path: "sendemail",
+        component: Email,
+        pathMatch: "full"
     },
 
     {
-        path:"",
-        component:Home,
-        pathMatch:"full"
+        path: "",
+        component: Home,
+        pathMatch: "full"
     },
     {
-        path:"guests",
-        component:GuestComponent,
-        pathMatch:"full"
-    },
-     {
-        path:"addguest",
-        component:AddGuestComponent,
-        pathMatch:"full"
+        path: "guests",
+        component: GuestComponent,
+        pathMatch: "full",
+        // canActivate: [authGuard]
     },
     {
-        path:"family",
-        component:Family,
-        pathMatch:"full"
+        path: "addguest",
+        component: AddGuestComponent,
+        pathMatch: "full",
+        // canActivate: [authGuard]
     },
     {
-        path:"expenses",
-        component:Expense,
-        pathMatch:"full"
+        path: "family",
+        component: Family,
+        pathMatch: "full",
+        // canActivate: [authGuard]
     },
     {
-        path:"dashboard",
-        component:Dashboard,
-        pathMatch:"full"
+        path: "expenses",
+        component: Expense,
+        pathMatch: "full",
+        // canActivate: [authGuard]
+    },
+    {
+        path: "dashboard",
+        component: Dashboard,
+        pathMatch: "full",
+        // canActivate: [authGuard]
+    },
+    {
+        path: "register",
+        component: Register,
+        pathMatch: "full"
+    },
+    {
+        path: "login",
+        component: Login,
+        pathMatch: "full"
     }
 
 ];
