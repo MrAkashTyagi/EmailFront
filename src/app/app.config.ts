2d7c7b4f409b30e0a70c022cdbd3c97d82ab6e05
MrAkashTyagi <34620687+MrAkashTyagi@users.noreply.github.com>
2026-09-16 18:26:55 +0530

    Update app.config.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,19 +3,32 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+
 
 import {
 provideNativeDateAdapter
 } from '@angular/material/core'; 
+import { authInterceptor } from './interceptors/auth-interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideRouter(routes),
-    provideClientHydration(withEventReplay()),
-    provideHttpClient(),
-    provideNativeDateAdapter()
-    
-  ]
+
+  provideBrowserGlobalErrorListeners(),
+
+  provideRouter(routes),
+
+  provideClientHydration(
+    withEventReplay()
+  ),
+
+  provideHttpClient(
+    withInterceptors([
+      authInterceptor
+    ])
+  ),
+
+  provideNativeDateAdapter()
+
+]
 };
