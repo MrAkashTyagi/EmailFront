21e8815a00361545c31ddd71a46b9095dd719017
MrAkashTyagi <34620687+MrAkashTyagi@users.noreply.github.com>
2026-09-18 23:00:30 +0530

    Update app.routes.server.ts

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,8 +1,47 @@
-import { RenderMode, ServerRoute } from '@angular/ssr';
+import {
+  RenderMode,
+  ServerRoute
+} from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: '',
+    renderMode: RenderMode.Client
+  },
+  {
+    path: 'login',
+    renderMode: RenderMode.Client
+  },
+  {
+    path: 'register',
+    renderMode: RenderMode.Client
+  },
+  {
+    path: 'dashboard',
+    renderMode: RenderMode.Client
+  },
+  {
+    path: 'guests',
+    renderMode: RenderMode.Client
+  },
+  {
+    path: 'family',
+    renderMode: RenderMode.Client
+  },
+  {
+    path: 'expenses',
+    renderMode: RenderMode.Client
+  },
+  {
+    path: 'addguest',
+    renderMode: RenderMode.Client
+  },
+  {
+    path: 'sendemail',
+    renderMode: RenderMode.Client
+  },
+  {
     path: '**',
-    renderMode: RenderMode.Prerender
+    renderMode: RenderMode.Client
   }
 ];
