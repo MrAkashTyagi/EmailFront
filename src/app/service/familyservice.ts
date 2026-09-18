74290dc6effcb063a5c6fa80d603c0d4dec1c797
MrAkashTyagi <34620687+MrAkashTyagi@users.noreply.github.com>
2026-09-18 23:06:53 +0530

    Update familyservice.ts

--- src/app/service/familyservice.ts
+++ src/app/service/familyservice.ts
@@ -1,69 +1,57 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { text } from 'stream/consumers';
 
 @Injectable({
   providedIn: 'root',
 })
 export class Familyservice {
 
-  constructor(private http: HttpClient) { }
+  private http =
+    inject(HttpClient);
+
+  private readonly baseUrl = "http://localhost:8090"
 
 
-  private baseUrl: string = "http://localhost:8090"
-
-  // getData(): Observable<string> {
-  //   return this.http.get(`${this.baseUrl}/family`, { responseType: 'text' });
-  // }
-
-  getData(): Observable<any> {
+  getFamilies(): Observable<any> {
     return this.http.get<any>(
       `${this.baseUrl}/family`
     );
   }
 
-  saveFamily(familyData: any): Observable<any> {
-    return this.http.post(`${this.baseUrl}/family`, familyData);
+  saveFamily(
+    familyData: any
+  ): Observable<any> {
+
+    return this.http.post<any>(
+      `${this.baseUrl}/family`,
+      familyData
+    );
+
   }
 
 
   deleteFamily(id: number): Observable<any> {
-    // Yeh URL database se us specific id ki family ko delete karega
     return this.http.delete<any>(`${this.baseUrl}/family/${id}`);
   }
 
 
   updateFamily(familyData: any): Observable<any> {
-    console.log("Service se API call ja rahi hai update ke liye:", familyData);
     return this.http.put<any>(`${this.baseUrl}/family/${familyData.id}`, familyData);
   }
-
-  // get paginated family
-
-  // getFamilyPaginated(page: number, size: number, search: string): Observable<any> {
-  // const params = new HttpParams()
-  // .set('page', page.toString())
-  // .set('size', size.toString())
-  // .set('search', search)
-
-  // return this.http.get<any>(`${this.baseUrl}/family`, {params});  
-  // }
 
   getFamilyPaginated(
     page: number,
     size: number,
-    search: string
-  ) {
-
+    search: string = ''
+  ): Observable<any> {
 
     const params = new HttpParams()
       .set('page', page.toString())
       .set('size', size.toString())
       .set('search', search)
 
-
-    return this.http.get(
+    return this.http.get<any>(
       `${this.baseUrl}/family`,
       { params }
     );
@@ -89,7 +77,7 @@ export class Familyservice {
       file
     );
 
-    return this.http.post(
+    return this.http.post<any>(
       `${this.baseUrl}/dataDump/upload`,
       formData
     );
