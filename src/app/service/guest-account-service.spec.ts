ef1539d78096bd3bcd12280612907d0f59b94460
MrAkashTyagi <34620687+MrAkashTyagi@users.noreply.github.com>
2026-09-22 17:12:06 +0530

    Create guest-account-service.spec.ts

---
+++ src/app/service/guest-account-service.spec.ts
@@ -0,0 +1,16 @@
+import { TestBed } from '@angular/core/testing';
+
+import { GuestAccountService } from './guest-account-service';
+
+describe('GuestAccountService', () => {
+  let service: GuestAccountService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(GuestAccountService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
