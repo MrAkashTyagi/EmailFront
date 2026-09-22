eb9d48b06c0433e613c0cd16900ec79da3433a43
MrAkashTyagi <34620687+MrAkashTyagi@users.noreply.github.com>
2026-09-22 17:16:59 +0530

    Create create-guest-account-dialog.ts

---
+++ src/app/components/create-guest-account-dialog/create-guest-account-dialog.ts
@@ -0,0 +1,118 @@
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatDialogRef } from '@angular/material/dialog';
+import { GuestAccountService } from '../../service/guest-account-service';
+import { MatIconModule } from '@angular/material/icon';
+
+
+
+import { CommonModule } from '@angular/common';
+
+import { MatDialogModule } from '@angular/material/dialog';
+
+import { MatFormFieldModule } from '@angular/material/form-field';
+
+import { MatInputModule } from '@angular/material/input';
+
+import { MatButtonModule } from '@angular/material/button';
+
+
+
+@Component({
+  selector: 'app-create-guest-account-dialog',
+  standalone: true,
+
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule
+  ],
+
+  templateUrl: './create-guest-account-dialog.html',
+  styleUrl: './create-guest-account-dialog.css'
+})
+export class CreateGuestAccountDialog {
+
+  private fb = inject(FormBuilder);
+
+  private guestAccountService =
+    inject(
+      GuestAccountService
+    );
+
+  private dialogRef =
+    inject(
+      MatDialogRef<CreateGuestAccountDialog>
+    );
+
+  form =
+    this.fb.group({
+
+      name: [
+        '',
+        Validators.required
+      ],
+
+      email: [
+        '',
+        [
+          Validators.required,
+          Validators.email
+        ]
+      ],
+
+      password: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(6)
+        ]
+      ]
+    });
+
+  createGuestAccount(): void {
+
+    if (
+      this.form.invalid
+    ) {
+
+      this.form.markAllAsTouched();
+
+      return;
+    }
+
+    this.guestAccountService
+      .createGuestAccount(
+        this.form.getRawValue() as any
+      )
+      .subscribe({
+
+        next: response => {
+
+          alert(
+            `Guest Account Created\n\nEmail: ${response.email}`
+          );
+
+          this.dialogRef.close(
+            true
+          );
+        },
+
+        error: error => {
+
+          console.error(
+            error
+          );
+
+          alert(
+            'Failed to create guest account'
+          );
+        }
+      });
+  }
+}
